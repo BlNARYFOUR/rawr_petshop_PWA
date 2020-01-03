eea27de7ed025e112980511102055053ceeff172
Brend Lambert <lambertbrend@gmail.com>
2020-01-03 17:43:48 +0100

    checkout done

--- src/app/pages/checkout/checkout.component.ts
+++ src/app/pages/checkout/checkout.component.ts
@@ -13,7 +13,13 @@ import {OrderService} from "../../services/order.service";
 export class CheckoutComponent implements OnInit {
 
     countryList: any = [];
+    shippingCost: number = 0;
     orderPlaced: boolean = false;
+    paymentMethod: any;
+    checkoutData: any;
+    showSuccess: boolean = false;
+    showError: boolean = false;
+    orderKey: string;
 
     constructor(private _router: Router,
                 private _orderService: OrderService) { }
@@ -26,6 +32,7 @@ export class CheckoutComponent implements OnInit {
             // this._router.navigateByUrl("/cart");
         } else {
             this.showCountryList();
+            this.getPaymentMethodDetails();
         }
     }
 
@@ -36,7 +43,8 @@ export class CheckoutComponent implements OnInit {
     showCountryList = () => {
         this._orderService.getCountryList().subscribe({
             next: (data: any) => {
-                console.log(data);
+                this.countryList = data;
+                this.updateShippingCost();
             },
             error: (data: any) => {
                 console.log(data.error.error);
@@ -47,4 +55,142 @@ export class CheckoutComponent implements OnInit {
             }
         });
     };
+
+    getSubTotal = () => {
+        let subtotal = 0;
+        CartService.products.forEach((product) => {
+            subtotal += product['price'] * product['cart_amount'];
+        });
+
+        return subtotal;
+    };
+
+    getPaymentMethodDetails = () => {
+        this._orderService.getPaymentMethod(document.querySelector("#payment_method").value).subscribe({
+            next: (data: any) => {
+                this.paymentMethod = data;
+            },
+            error: (data: any) => {
+                console.log(data.error.error);
+
+                if (data.error) {
+                    // TODO
+                }
+            }
+        });
+    };
+
+    getInternationalShipping = () => {
+        this._orderService.getInternationalShipping().subscribe({
+            next: (data: any) => {
+                this.shippingCost = parseFloat(data[0]['settings']['cost']['value']);
+            },
+            error: (data: any) => {
+                console.log(data.error.error);
+
+                if (data.error) {
+                    // TODO
+                }
+            }
+        });
+    };
+
+    getNationalShipping = () => {
+        this._orderService.getNationalShipping().subscribe({
+            next: (data: any) => {
+                this.shippingCost = parseFloat(data[0]['settings']['cost']['value']);
+            },
+            error: (data: any) => {
+                console.log(data.error.error);
+
+                if (data.error) {
+                    // TODO
+                }
+            }
+        });
+    };
+
+    updateShippingCost = () => {
+        let countryCode = document.querySelector("#country_shipping").value;
+
+        if(countryCode === "BE") {
+            this.getNationalShipping();
+        } else {
+            this.getInternationalShipping();
+        }
+    };
+
+    buildCheckoutData = () => {
+        this.checkoutData = {
+            "payment_method": this.paymentMethod.id,
+            "payment_method_title": this.paymentMethod.title,
+            "set_paid": false,
+            "billing": this.getFormAsJson('billing_address_form'),
+            "shipping": this.getFormAsJson('shipping_address_form'),
+            "line_items": this.getBasicCartInfo(),
+            "shipping_lines": [
+                {
+                    "method_id": "flat_rate",
+                    "method_title": "Flat Rate",
+                    "total": this.shippingCost.toString()
+                }
+            ]
+        }
+    };
+
+    getFormAsJson = (id: string) => {
+        let data = {};
+        let fields = document.querySelector('#' + id).elements;
+
+        for(let i=0; i < fields.length; i++) {
+            data[fields[i].name] = fields[i].value;
+        }
+
+        return data;
+    };
+
+    getBasicCartInfo = () => {
+        let cartItems = [];
+
+        CartService.products.forEach((p) => {
+            cartItems.push({
+                product_id: p.id,
+                quantity: p.cart_amount
+            });
+        });
+
+        return cartItems;
+    };
+
+    getFieldValue = (id: string) => {
+        return document.querySelector('#' + id).value;
+    };
+
+    submitOrder = () => {
+        this._orderService.postNewOrder(this.checkoutData).subscribe({
+            next: (data: any) => {
+                this.orderKey = data['order_key'].split('_')[2];
+                this.showSuccess = true;
+                CartService.products = [];
+                this.orderPlaced = false;
+            },
+            error: (data: any) => {
+                this.showError = true;
+                this.orderPlaced = false;
+                console.log(data.error.error);
+
+                if (data.error) {
+                    // TODO
+                }
+            }
+        });
+    };
+
+    onTryOrder() {
+        this.orderPlaced = true;
+        this.buildCheckoutData();
+        this.submitOrder();
+        console.log(this.checkoutData);
+        console.log(CartService.products);
+    }
 }
--- src/app/services/order.service.ts
+++ src/app/services/order.service.ts
@@ -9,6 +9,10 @@ export class OrderService {
 
     getCountryListUrl = environment.apiUrl + 'data/countries';
     getInternationalShippingPriceUrl = environment.apiUrl + 'shipping/zones/0/methods';
+    getNationalShippingPriceUrl = environment.apiUrl + 'shipping/zones/1/methods';
+    getPaymentMethodUrl = environment.apiUrl + "payment_gateways/";
+    postOrderUrl = environment.apiUrl + "orders";
+
 
     urlAuthAddOn = '?consumer_key=' + environment.consumerKey + '&consumer_secret=' + environment.consumerSecret;
 
@@ -18,5 +22,17 @@ export class OrderService {
 
     getInternationalShipping() {
         return this._http.get(this.getInternationalShippingPriceUrl + this.urlAuthAddOn);
+    }
+
+    getNationalShipping() {
+        return this._http.get(this.getNationalShippingPriceUrl + this.urlAuthAddOn);
+    }
+
+    getPaymentMethod(id: string) {
+        return this._http.get(this.getPaymentMethodUrl + id + this.urlAuthAddOn);
+    }
+
+    postNewOrder(data: any) {
+        return this._http.post(this.postOrderUrl + this.urlAuthAddOn, data);
     }
 }
